2b5c8adc7c5768d2a917e3ad8183a52e479fa745
Futile <felixr@archlinux.info>
2015-03-28 21:58:25 +0100

    very basic window loop, quit on escape

---
+++ src/application/gamestate.rs
@@ -0,0 +1,41 @@
+use glium::{self, Surface};
+use glutin::{self, ElementState, VirtualKeyCode};
+
+use std::thread;
+use std::time::duration::Duration;
+
+use util::{State};
+use application::AppTransition;
+
+pub struct GameState {
+    display: glium::Display,
+}
+
+impl GameState {
+    pub fn new(display: glium::Display) -> GameState {
+        GameState{
+            display: display,
+        }
+    }
+}
+
+impl State<AppTransition> for GameState {
+    fn run(self: Box<Self>) -> AppTransition {
+        loop {
+            for event in self.display.poll_events() {
+                let mut target = self.display.draw();
+                target.clear_color(0.0, 0.0, 0.0, 0.0);
+                target.finish();
+
+                match event {
+                    glutin::Event::Closed |
+                    glutin::Event::KeyboardInput(ElementState::Released, _, Some(VirtualKeyCode::Escape))
+                        => return AppTransition::Shutdown,
+                    _ => ()
+                }
+
+                thread::sleep(Duration::milliseconds(17));
+            }
+        }
+    }
+}
--- src/application/mod.rs
+++ src/application/mod.rs
@@ -3,11 +3,16 @@ use glutin;
 
 use util::{State, Transition};
 
+pub use self::gamestate::GameState;
+
+mod gamestate;
+
 pub enum AppTransition {
     Startup,
     StartGame(glium::Display),
     Shutdown,
     TerminateApplication
+
 }
 
 impl Transition for AppTransition {
@@ -18,7 +23,7 @@ impl Transition for AppTransition {
     fn create_state(self) -> Option<Box<State<AppTransition>>> {
         match self {
             AppTransition::Startup => Some(Box::new(StartupState)),
-            AppTransition::StartGame(d) => Some(Box::new(GameState{display: d})),
+            AppTransition::StartGame(d) => Some(Box::new(GameState::new(d))),
             AppTransition::Shutdown => Some(Box::new(ShutdownState)),
             AppTransition::TerminateApplication => None
         }
@@ -37,16 +42,6 @@ impl State<AppTransition> for StartupState {
             .build_glium().unwrap();
 
         AppTransition::StartGame(display)
-    }
-}
-
-pub struct GameState {
-    display: glium::Display,
-}
-
-impl State<AppTransition> for GameState {
-    fn run(self: Box<Self>) -> AppTransition {
-        AppTransition::Shutdown
     }
 }
 
