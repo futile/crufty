ff3ee2c4912d5c61960ea2f245f181de44e2d540
Futile <felixr@archlinux.info>
2015-07-15 15:28:12 +0200

    add InputContext and MappedInputs

--- src/application/input/intents.rs
+++ src/application/input/intents.rs
@@ -1,3 +1,4 @@
+#[derive(Copy, Clone, Eq, PartialEq, Hash)]
 pub enum InputIntent {
     //
 }
--- src/application/input/mod.rs
+++ src/application/input/mod.rs
@@ -1,6 +1,7 @@
 use glium::glutin::{ElementState, VirtualKeyCode};
 
 use std::collections::HashSet;
+use std::collections::HashMap;
 
 mod intents;
 
@@ -27,4 +28,50 @@ impl KeyboardState {
     pub fn is_pressed(&self, vkc: VirtualKeyCode) -> bool {
         return self.keys.contains(&vkc);
     }
+}
+
+type InputContext = HashMap<VirtualKeyCode, InputIntent>;
+
+pub struct MappedInputs {
+    intent_to_key: HashMap<InputIntent, VirtualKeyCode>,
+    consumed_keys: HashSet<VirtualKeyCode>,
+}
+
+impl MappedInputs {
+    pub fn new() -> MappedInputs {
+        MappedInputs {
+            intent_to_key: HashMap::new(),
+            consumed_keys: HashSet::new(),
+        }
+    }
+
+    pub fn add_input(&mut self, intent: InputIntent, key: VirtualKeyCode) {
+        self.intent_to_key.insert(intent, key);
+    }
+
+    pub fn has_intent(&self, intent: &InputIntent) -> bool {
+        if let Some(vkc) = self.intent_to_key.get(intent) {
+            return !self.consumed_keys.contains(vkc);
+        }
+
+        return false;
+    }
+
+    // returns `true` if the intent wasn't consumed before
+    pub fn consume_intent(&mut self, intent: &InputIntent) -> bool {
+        if let Some(vkc) = self.intent_to_key.get(intent) {
+            return self.consumed_keys.insert(*vkc);
+        }
+
+        return false;
+    }
+
+    pub fn clear(&mut self) {
+        self.intent_to_key.clear();
+        self.consumed_keys.clear();
+    }
+}
+
+pub struct InputManager {
+    input_contexts: Vec<InputContext>,
 }
