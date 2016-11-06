f94d8a0119329168df38edfa0f41ec71f273327e
Futile <felixr@archlinux.info>
2016-11-06 22:36:30 +0100

    add sending to UdpConnection

--- src/net/udp.rs
+++ src/net/udp.rs
@@ -1,7 +1,12 @@
 use std::net::{SocketAddr, UdpSocket};
-use std::time::Duration;
+use std::time::{Duration, Instant};
 use std::num::Wrapping;
 use std::ops::Sub;
+use std::io::{self, Cursor};
+use std::io::prelude::Write;
+use std::collections::VecDeque;
+
+use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
 
 // this is an implementation following http://gafferongames.com/networking-for-game-programmers/reliability-and-flow-control/
 
@@ -21,6 +26,10 @@ impl SequenceNumber {
 
     pub fn first() -> SequenceNumber {
         SequenceNumber::new(Self::FIRST_SEQUENCE_NUMBER)
+    }
+
+    pub fn write_to_packet<W: Write>(&self, writer: &mut W) -> io::Result<()> {
+        writer.write_u16::<EncodingType>((self.0).0)
     }
 }
 
@@ -70,14 +79,14 @@ struct AckControl {
 }
 
 impl AckControl {
-    pub fn new() -> AckControl {
+    fn new() -> AckControl {
         AckControl {
             remote_sequence_number: None,
             earlier_acks: 0,
         }
     }
 
-    pub fn ack(&mut self, seq_num: SequenceNumber) {
+    fn ack(&mut self, seq_num: SequenceNumber) {
         let remote_sequence_number = match self.remote_sequence_number {
             Some(ref mut rsn) => rsn,
             None => { self.remote_sequence_number = Some(seq_num); return; }
@@ -85,8 +94,6 @@ impl AckControl {
 
         // this substraction is wraparound aware
         let diff: i32 = seq_num - (remote_sequence_number as &SequenceNumber);
-
-        println!("diff: {}", diff);
 
         if diff > 0 {
             // a newer, more recent remote sequence number was received
@@ -134,7 +141,53 @@ impl AckControl {
             return;
         }
     }
+
+    fn write_to_packet<W: Write>(&self, writer: &mut W) -> io::Result<()> {
+        let rsn = match self.remote_sequence_number {
+            Some(rsn) => rsn,
+            None => 0.into(),
+        };
+
+        rsn.write_to_packet(writer)?;
+        writer.write_u32::<EncodingType>(self.earlier_acks)
+    }
 }
+
+#[derive(Debug)]
+struct Buffer(Vec<u8>);
+
+impl Buffer {
+    fn new() -> Buffer {
+        Buffer(Vec::new())
+    }
+
+    fn take(&mut self) -> Vec<u8> {
+        ::std::mem::replace(&mut self.0, Vec::new())
+    }
+
+    fn done(&mut self, buf: Vec<u8>) {
+        if buf.capacity() > self.0.capacity() {
+            self.0 = buf;
+
+            // invariant: buffer has to stay cleared
+            self.0.clear();
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct InFlightInfo {
+    seq_num: SequenceNumber,
+    sent_time: Instant,
+    msg_id: MessageId,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub struct MessageId(u64);
+
+const MAGIC_PROTOCOL_ID: u32 = 0xABFECDFE;
+
+type EncodingType = BigEndian;
 
 pub struct UdpConnection {
     socket: UdpSocket,
@@ -142,6 +195,10 @@ pub struct UdpConnection {
     next_local_sequence_number: SequenceNumber,
     ack_control: AckControl,
     rtt: Duration,
+
+    next_message_id: MessageId,
+    buffer: Buffer,
+    pending_acks: VecDeque<InFlightInfo>,
 }
 
 impl UdpConnection {
@@ -154,13 +211,61 @@ impl UdpConnection {
             next_local_sequence_number: SequenceNumber::first(),
             ack_control: AckControl::new(),
             rtt: Duration::new(0, 0),
+
+            next_message_id: MessageId(0),
+            buffer: Buffer::new(),
+            pending_acks: VecDeque::new(),
         }
+    }
+
+    fn write_header<W: Write>(&mut self, writer: &mut W) -> io::Result<()> {
+        // write magic protocol id
+        writer.write_u32::<EncodingType>(MAGIC_PROTOCOL_ID)?;
+
+        // write sequence number
+        self.next_local_sequence_number.write_to_packet(writer)?;
+
+        // write acks
+        self.ack_control.write_to_packet(writer)
+    }
+
+    pub fn send_bytes(&mut self, msg: &[u8]) -> MessageId {
+        let mut buffer = self.buffer.take();
+
+        // write header to buffer
+        self.write_header(&mut buffer).unwrap();
+
+        // write msg to buffer
+        buffer.write_all(msg).unwrap();
+
+        // send packet
+        let sent_count = self.socket.send_to(&buffer, &self.remote_addr).unwrap();
+        assert_eq!(sent_count, buffer.len(), "only a partial send occured, should not happen??");
+
+        // create MessageId
+        let msg_id = self.next_message_id;
+        self.next_message_id.0 += 1;
+
+        // add packet info (seq num, sent-timestamp, message id) to pending acks
+        self.pending_acks.push_back(InFlightInfo {
+            seq_num: self.next_local_sequence_number,
+            sent_time: Instant::now(),
+            msg_id: msg_id,
+        });
+
+        // increase next sequence number
+        self.next_local_sequence_number += 1.into();
+
+        // give buffer back
+        self.buffer.done(buffer);
+
+        return msg_id;
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{SequenceNumber, AckControl};
+    use super::{UdpConnection, SequenceNumber, AckControl};
 
     #[test]
     fn test_sub_seq_nums() {
