aa0413eadc626df0bc2492e9f6f4716f64651af4
Futile <felixr@archlinux.info>
2016-11-08 20:55:50 +0100

    test programs for client and server

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -12,18 +12,43 @@ use std::net::SocketAddr;
 use std::thread;
 use std::time::Duration;
 
-use crufty::net::client::ClientConnection;
+// use crufty::net::client::ClientConnection;
+
+// fn main() {
+//     let mut conn = ClientConnection::new().expect("could not create client connection");
+
+//     conn.start_connect(&SocketAddr::from_str("127.0.0.1:13625").unwrap());
+
+//     loop {
+//         if let Some(event) = conn.handle() {
+//             println!("event: {:?}", event);
+//         }
+
+//         thread::sleep(Duration::from_millis(100));
+//     }
+// }
+
+use crufty::net::udp::{UdpConnection};
+
+use std::time::Instant;
 
 fn main() {
-    let mut conn = ClientConnection::new().expect("could not create client connection");
+    let mut conn = UdpConnection::new(&"127.0.0.1:12365".parse().unwrap(),
+                                      &"127.0.0.1:12366".parse().unwrap(),
+                                      Duration::from_secs(3));
 
-    conn.start_connect(&SocketAddr::from_str("127.0.0.1:13625").unwrap());
+    // interval at which we send messages
+    let send_interval = Duration::from_secs(2);
+
+    let mut event_buffer = Vec::new();
 
     loop {
-        if let Some(event) = conn.handle() {
-            println!("event: {:?}", event);
-        }
+        let msg = "Hello, Udp!".as_bytes();
+        conn.send_bytes(&msg);
+        println!("msg: {:?}", msg);
 
-        thread::sleep(Duration::from_millis(100));
+        conn.update(Instant::now() + send_interval, &mut event_buffer);
+
+        println!("{:?}", event_buffer);
     }
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -3,19 +3,48 @@ extern crate crufty;
 use std::net::SocketAddr;
 use std::str::FromStr;
 use std::thread;
-use std::time::Duration;
+use std::time::{Instant, Duration};
 
-use crufty::net::server::ServerBind;
+// use crufty::net::server::ServerBind;
+
+// fn main() {
+//     let mut bind = ServerBind::new(&SocketAddr::from_str("127.0.0.1:13625").unwrap())
+//         .expect("could not create ServerBind");
+
+//     loop {
+//         if let Some(event) = bind.handle() {
+//             println!("event: {:?}", event);
+//         }
+
+//         thread::sleep(Duration::from_millis(100));
+//     }
+// }
+
+use crufty::net::udp::{UdpConnection, UdpConnectionEvent};
 
 fn main() {
-    let mut bind = ServerBind::new(&SocketAddr::from_str("127.0.0.1:13625").unwrap())
-        .expect("could not create ServerBind");
+    let mut conn = UdpConnection::new(&"127.0.0.1:12366".parse().unwrap(),
+                                      &"127.0.0.1:12365".parse().unwrap(),
+                                      Duration::from_secs(3));
+
+    let mut event_buffer = vec![];
 
     loop {
-        if let Some(event) = bind.handle() {
-            println!("event: {:?}", event);
+        conn.update(Instant::now() + Duration::from_secs(3), &mut event_buffer);
+
+        for event in event_buffer.drain(..) {
+            match event {
+                UdpConnectionEvent::MessageReceived(msg) => {
+                    println!("msg raw: {:?}", msg);
+                    let msg_str = ::std::str::from_utf8(&msg).unwrap();
+                    println!("Message: {}", msg_str);
+
+                    conn.send_bytes(&format!("Ping: '{}'", msg_str).as_bytes());
+                }
+                mto @ UdpConnectionEvent::MessageTimedOut(_) => println!("{:?}", mto),
+            }
         }
 
-        thread::sleep(Duration::from_millis(100));
+        println!("{:?}", event_buffer);
     }
 }
