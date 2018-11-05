c458c6ec75f1060820ff4532fe6b51ffd77fae2b
Felix Rath <felix.rath@rwth-aachen.de>
2018-11-05 22:17:42 +0100

    start on tracking PeerData

--- src/net/mod.rs
+++ src/net/mod.rs
@@ -1,9 +1,11 @@
 use std::net::Ipv4Addr;
+use std::time::Instant;
 
 use ecs::World;
 
-use enet::{self, Enet};
+use enet::{self, Enet, Event};
 
+use crate::components::LevelChangedFlags;
 use crate::systems::LevelSystems;
 
 lazy_static! {
@@ -12,14 +14,20 @@ lazy_static! {
 
 const PORT: u16 = 9001;
 
+#[derive(Debug, Default)]
+struct PeerData {
+    changes: LevelChangedFlags,
+}
+
 pub struct Host {
-    enet_host: enet::Host<()>,
+    enet_host: enet::Host<PeerData>,
+    last_maintain: Instant,
 }
 
 impl Host {
     pub fn new() -> Host {
         let enet_host = ENET
-            .create_host::<()>(
+            .create_host(
                 Some(&enet::Address::new(Ipv4Addr::LOCALHOST, PORT)),
                 255,
                 enet::ChannelLimit::Maximum,
@@ -28,13 +36,31 @@ impl Host {
             )
             .unwrap();
 
-        Host { enet_host }
+        Host {
+            enet_host,
+            last_maintain: Instant::now(),
+        }
     }
 
     pub fn maintain(&mut self, world: &mut World<LevelSystems>) {
-        if let Some(event) = self.enet_host.service(0).unwrap() {
-            dbg!(event);
-        }
+        fn loop_body(mut event: Event<'_, PeerData>) {
+            dbg!(&event);
+
+            match event {
+                Event::Connect(ref mut peer) => peer.set_data(Some(PeerData::default())),
+                _ => (),
+            }
+        };
+
+        if let Some(mut event) = self.enet_host.service(0).unwrap() {
+            loop_body(event);
+        };
+
+        while let Some(mut event) = self.enet_host.check_events().unwrap() {
+            loop_body(event);
+        };
+
+        self.last_maintain = Instant::now();
     }
 }
 
